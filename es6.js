bb6ef12f19e9b56ec4d9ff20457f8986f7c0ebd2
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-17 23:28:38 +0600

    js spread operator

--- es6.js
+++ es6.js
@@ -9,5 +9,12 @@ const about = `My Name is ${student.name} age of ${student.age} has number ${stu
 
 // 2. arrow function
 const arrFun = () => {
-    
+
 }
+
+// 3. spread operator
+//create a new array from an older array and add an element
+const currentNum = [...numbers, 44];
+numbers.push(99);
+console.log(numbers);
+console.log(currentNum);
