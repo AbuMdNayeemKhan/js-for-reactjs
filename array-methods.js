3f8efe9cf1331632def42a50e2086791c00caeb5
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-18 02:52:52 +0600

    map

---
+++ array-methods.js
@@ -0,0 +1,10 @@
+const products = [
+    {name: 'laptop', price: '3200', brand: 'lenovo', color: 'silver'},
+    {name: 'phone', price: '3600', brand: 'apple', color: 'black'},
+    {name: 'soundbox', price: '2400', brand: 'sony', color: 'black'},
+    {name: 'headphone', price: '300', brand: 'a4tech', color: 'white'},
+    {name: 'camera', price: '200', brand: 'canon', color: 'black'},
+]
+products.map((pro)=>{
+    console.log(pro.name);
+});
