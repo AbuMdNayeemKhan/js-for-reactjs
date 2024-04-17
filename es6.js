88daa5be3efd806fbdeabe4c63322b18e5e383ec
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-17 23:14:13 +0600

    template string and arrow function

---
+++ es6.js
@@ -0,0 +1,13 @@
+// 1. template string
+const student = {
+    name: "Nayeem",
+    age: 32,
+    frientd: ['Nayeem', 'Dola', 'Asia', 'Shokal', 'Rasel'],
+}
+const numbers = [10, 43, 25, 65, 67];
+const about = `My Name is ${student.name} age of ${student.age} has number ${student.frientd[2]} also liked`;
+
+// 2. arrow function
+const arrFun = () => {
+    
+}
